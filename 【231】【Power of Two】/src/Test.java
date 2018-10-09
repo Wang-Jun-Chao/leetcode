74251ca76cb462e5d03a5ca8e6b5701c709671f8
王俊超 <wangjunchao@gf.com.cn>
2018-10-09 13:06:54 +0800

    commit

---
+++ 【231】【Power of Two】/src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2018-10-09 09:28
+ **/
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.isPowerOfTwo(-1));
+        System.out.println(solution.isPowerOfTwo(0));
+        System.out.println(solution.isPowerOfTwo(1));
+        System.out.println(solution.isPowerOfTwo(2));
+        System.out.println(solution.isPowerOfTwo(16));
+        System.out.println(solution.isPowerOfTwo(218));
+    }
+}
---
+++ 【234】【Palindrome Linked List】/src/Test.java
@@ -0,0 +1,42 @@
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2018-10-09 11:10
+ **/
+public class Test {
+    public static void main(String[] args) {
+        test1();
+        test2();
+        test3();
+    }
+
+    public static void test1() {
+        Solution solution = new Solution();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        System.out.println(solution.isPalindrome(head));
+    }
+
+    public static void test2() {
+        Solution solution = new Solution();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next= new ListNode(2);
+        head.next.next.next= new ListNode(1);
+        head.next.next.next.next= new ListNode(2);
+        head.next.next.next.next.next= new ListNode(2);
+        head.next.next.next.next.next.next= new ListNode(1);
+        System.out.println(solution.isPalindrome(head));
+    }
+
+    public static void test3() {
+        Solution solution = new Solution();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next= new ListNode(2);
+        head.next.next.next= new ListNode(1);
+
+        System.out.println(solution.isPalindrome(head));
+    }
+}
