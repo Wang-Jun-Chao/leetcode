66b3a79d810fc281ad7817a69ae39e37ebcd5a3a
王俊超 <wangjunchao@gf.com.cn>
2018-10-10 10:27:53 +0800

    commit

---
+++ 【237】【Delete Node in a Linked List】/src/ListNode.java
@@ -0,0 +1,12 @@
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2018-10-10 09:58
+ **/
+public class ListNode {
+    int      val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
