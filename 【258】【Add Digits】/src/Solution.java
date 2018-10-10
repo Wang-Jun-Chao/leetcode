9ad71d796dcb249ae76c3285f9cb7ef4a6692e87
王俊超 <wangjunchao@gf.com.cn>
2018-10-10 13:49:55 +0800

    commit

--- 【258】【Add Digits】/src/Solution.java
+++ 【258】【Add Digits】/src/Solution.java
@@ -11,10 +11,11 @@ public class Solution {
 
         int result = 0;
 
-        while (num != 0) {
+        while (num > 0) {
             result += num % 10;
             num /= 10;
 
+            // 需要重新进行位相加
             if (num == 0 && result > 9) {
                 num = result;
                 result = 0;
