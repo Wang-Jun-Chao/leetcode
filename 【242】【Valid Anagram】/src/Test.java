eb56524bcb217a02f03660fed805566749fb1fbd
王俊超 <wangjunchao@gf.com.cn>
2018-10-10 10:38:12 +0800

    commit

---
+++ 【242】【Valid Anagram】/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2018-10-10 10:35
+ **/
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.isAnagram("anagram", "nagaram"));
+        System.out.println(solution.isAnagram("rat", "car"));
+    }
+}
