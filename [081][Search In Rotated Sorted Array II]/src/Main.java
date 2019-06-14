3095511ac32ecf8add8f9efffc5e52ad083848d7
Wang-Jun-Chao <shining-glory@qq.com>
2019-06-14 10:51:20 +0800

    commit

---
+++ [081][Search In Rotated Sorted Array II]/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Author: 王俊超
+ * Date: 2015-06-21
+ * Time: 16:38
+ * Declaration: All Rights Reserved !!!
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        System.out.println(solution.search(new int[]{1, 1, 3, 1}, 3));
+        System.out.println(solution.search(new int[]{5, 1, 3}, 3));
+    }
+}
