207a95b5cf526b546c630119bfd2fd1c23261cdb
Wang-Jun-Chao <shining-glory@qq.com>
2019-06-30 14:27:54 +0800

    commit

---
+++ [0404][Sum of Left Leaves]/src/Main.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2019-06-30 14:18
+ **/
+public class Main {
+    @Test
+    public void test1() {
+        TreeNode root = new TreeNode(3);
+        root.left  =new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Solution solution = new Solution();
+        Assert.assertEquals(24, solution.sumOfLeftLeaves(root));
+    }
+
+    @Test
+    public void test2() {
+        TreeNode root = new TreeNode(1);
+
+        Solution solution = new Solution();
+        Assert.assertEquals(0, solution.sumOfLeftLeaves(root));
+    }
+}
---
+++ [0404][Sum of Left Leaves]/src/Solution.java
@@ -0,0 +1,50 @@
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2019-06-30 14:11
+ **/
+public class Solution {
+    /**
+     * <pre>
+     * Find the sum of all left leaves in a given binary tree.
+     *
+     * Example:
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     *
+     * There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+     * </pre>
+     *
+     * @param root
+     * @return
+     */
+    public int sumOfLeftLeaves(TreeNode root) {
+        int[] result = new int[1];
+        sumOfLeftLeaves(root, false, result);
+        return result[0];
+    }
+
+    /**
+     * 计算所有左叶子的和
+     * @param root
+     * @param result
+     */
+    private void sumOfLeftLeaves(TreeNode root, boolean isLeftLeaf, int[] result) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (root.left == null && root.right==null && isLeftLeaf) {
+            result[0] += root.val;
+            return;
+        }
+
+        sumOfLeftLeaves(root.left, true, result);
+        sumOfLeftLeaves(root.right, false, result);
+    }
+}
