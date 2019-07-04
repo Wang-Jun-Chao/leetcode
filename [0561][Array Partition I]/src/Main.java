32d88f67984074fb52d1fdd7d28dd2d227703ec9
Wang-Jun-Chao <shining-glory@qq.com>
2019-07-04 08:56:57 +0800

    commit

---
+++ [0561][Array Partition I]/src/Main.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2019-07-04 08:49
+ **/
+public class Main {
+    @Test
+    public void test1(){
+        Solution solution = new Solution();
+        int[] arr = {1,4,3,2};
+        Assert.assertEquals(4, solution.arrayPairSum(arr));
+    }
+}
---
+++ [0561][Array Partition I]/src/Solution.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/array-partition-i/
+ * @author: wangjunchao(王俊超)
+ * @time: 2019-07-04 08:46
+ **/
+public class Solution {
+    /**
+     * <pre>
+     * Given an array of 2n integers, your task is to group these integers into n pairs of integer,
+     * say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n
+     * as large as possible.
+     *
+     * Example 1:
+     * Input: [1,4,3,2]
+     *
+     * Output: 4
+     * Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+     * Note:
+     * n is a positive integer, which is in the range of [1, 10000].
+     * All the integers in the array will be in the range of [-10000, 10000].
+     *
+     * 解题思路
+     * 先将数据组排序，取偶数位置的数相加，即为结果
+     * 证明略
+     * </pre>
+     * @param nums
+     * @return
+     */
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int result = 0;
+        for (int i = 0; i < nums.length; i+=2) {
+            result += nums[i];
+        }
+
+        return result;
+    }
+}
