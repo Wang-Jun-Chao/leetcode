8bd9a6e0a926955fe93f0ae82f2d2c6a4eed13e0
Wang-Jun-Chao <shining-glory@qq.com>
2019-07-07 12:37:26 +0800

    commit

---
+++ [0207][Course Schedule]/src/Main.java
@@ -0,0 +1,45 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2019-07-07 11:35
+ **/
+public class Main {
+    @Test
+    public void test1() {
+        Solution solution = new Solution();
+        int[][] arr = {{1, 0}};
+        Assert.assertEquals(true, solution.canFinish(2, arr));
+    }
+
+    @Test
+    public void test2() {
+        Solution solution = new Solution();
+        int[][] arr = {{1, 0}, {0, 1}};
+        Assert.assertEquals(false, solution.canFinish(2, arr));
+    }
+
+
+    @Test
+    public void test3() {
+        Solution solution = new Solution();
+        int[][] arr = {{0, 1}, {0, 2}, {1, 2}};
+        Assert.assertEquals(true, solution.canFinish(3, arr));
+    }
+
+    @Test
+    public void test4() {
+        Solution solution = new Solution();
+        int[][] arr = {{2, 0}, {1, 0}, {3, 1}, {3, 2}, {1, 3}};
+        Assert.assertEquals(false, solution.canFinish(4, arr));
+    }
+
+
+    @Test
+    public void test5() {
+        Solution solution = new Solution();
+        int[][] arr = {{1,0},{2,6},{1,7},{6,4},{7,0},{0,5}};
+        Assert.assertEquals(true, solution.canFinish(8, arr));
+    }
+}
---
+++ [0207][Course Schedule]/src/Solution.java
@@ -0,0 +1,108 @@
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2019-07-07 11:23
+ **/
+public class Solution {
+    /**
+     * <pre>
+     * There are a total of n courses you have to take, labeled from 0 to n-1.
+     *
+     * Some courses may have prerequisites, for example to take course 0 you have to first take
+     * course 1, which is expressed as a pair: [0,1]
+     *
+     * Given the total number of courses and a list of prerequisite pairs, is it possible for
+     * you to finish all courses?
+     *
+     * Example 1:
+     *
+     * Input: 2, [[1,0]]
+     * Output: true
+     * Explanation: There are a total of 2 courses to take.
+     *              To take course 1 you should have finished course 0. So it is possible.
+     * Example 2:
+     *
+     * Input: 2, [[1,0],[0,1]]
+     * Output: false
+     * Explanation: There are a total of 2 courses to take.
+     *              To take course 1 you should have finished course 0, and to take course 0 you should
+     *              also have finished course 1. So it is impossible.
+     * Note:
+     *
+     * The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read
+     * more about how a graph is represented.
+     * You may assume that there are no duplicate edges in the input prerequisites.
+     *
+     * 解题思路：
+     * 使用深度度优先遍历，记录每个访问过的点，如果遍历过程中再次遍历到了已经访问过的点，
+     * 说明有环状依赖，课程安排不合理
+     * </pre>
+     */
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // 说明没有课程依赖
+        if (prerequisites == null || prerequisites.length == 0) {
+            return true;
+        }
+
+
+        Map<Integer, List<Integer>> dependencies = new HashMap<>();
+
+        for (int[] arr : prerequisites) {
+            List<Integer> val = dependencies.computeIfAbsent(arr[0], k -> new LinkedList<>());
+            val.add(arr[1]);
+        }
+
+        boolean[] visit = new boolean[numCourses];
+        Deque<Integer> path = new LinkedList<>();
+        for (int i = 0; i < visit.length; i++) {
+            if (!visit[i]) {
+                if (!dfs(i, dependencies, visit, path)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 判断从i课程序开始，是否存在环
+     *
+     * @param course
+     * @param dependencies
+     * @param visit
+     * @return
+     */
+    private boolean dfs(int course, Map<Integer, List<Integer>> dependencies, boolean[] visit, Deque<Integer> path) {
+        List<Integer> dependency = dependencies.get(course);
+
+        if (dependency == null) {
+            return true;
+        }
+
+        if (path.contains(course)) {
+            return false;
+        }
+
+        path.addLast(course);
+
+
+        for(int i: dependency) {
+            boolean result = dfs(i, dependencies, visit, path);
+            if (!result) {
+                return false;
+            }
+        }
+
+        visit[course] = true;
+        path.removeLast();
+
+        return true;
+    }
+}
