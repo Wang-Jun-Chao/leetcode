3f5a8449149e5e1e824f73708c9c8710c45ff8b6
Wang-Jun-Chao <shining-glory@qq.com>
2019-07-09 22:15:07 +0800

    commit

--- [0979][Distribute Coins in Binary Tree]/src/Main.java
+++ [0979][Distribute Coins in Binary Tree]/src/Main.java
@@ -1,7 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author: wangjunchao(王俊超)
  * @time: 2019-07-09 21:10
  **/
 public class Main {
-    private final static Logger logger = LoggerFactory.getLogger(Main.class);
+    @Test
+    public void test1() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(0);
+
+        Solution solution = new Solution();
+        Assert.assertEquals(2, solution.distributeCoins(root));
+    }
 }
--- [0979][Distribute Coins in Binary Tree]/src/Solution.java
+++ [0979][Distribute Coins in Binary Tree]/src/Solution.java
@@ -1,4 +1,5 @@
 /**
+ * https://leetcode.com/problems/distribute-coins-in-binary-tree/
  * @author: wangjunchao(王俊超)
  * @time: 2019-07-09 18:52
  **/
@@ -6,20 +7,20 @@ public class Solution {
 
     private int moves = 0;
 
+    /**
+     * <pre>
+     * 解题思路：这个题目有点意思，我的方法是“借”的思想。对于任意一个叶子节点，如果val是0，
+     * 那么表示要向其父节点取一个coin，那么parent.val -= 1, moves += 1；如果是叶子节点
+     * 的val大于1，那么表示要给父节点val-1个coin，同时moves += (val-1)。当然这两种情况
+     * 可以用通用的表达式：move += abs(node.val - 1), parent.val += (node.val - 1)。
+     * 按照后序遍历的方式即可算出总的move次数。
+     *
+     * https://www.cnblogs.com/seyjs/p/10369614.html
+     * </pre>
+     * @param root
+     * @return
+     */
     public int distributeCoins(TreeNode root) {
-        /**
-         * <pre>
-         * 一个节点有几种情况：
-         * 1、没有左和右孩子
-         *      1.1、自己的val为0，需要从上面借1个coin
-         *      1.2、自己的val为1，不需要借coin
-         *      1.3、自己的val>1，需要向父结点贡献 val-1个coin
-         * 2、只有左或者右孩子
-         *      2.1、自己的
-         * </pre>
-         */
-
-
         if (root == null) {
             return 0;
         }
@@ -30,7 +31,7 @@ public class Solution {
     }
 
     /**
-     *
+     * 自底向上，对每一个节点，只能从父结点借，或者向父节点上交coin
      * @param node
      * @param parent
      */
@@ -41,15 +42,17 @@ public class Solution {
         }
 
         if (node.left != null) {
-            distributeCoins(node.left, parent);
+            distributeCoins(node.left, node);
         }
 
         if (node.right != null) {
-            distributeCoins(node.right, parent);
+            distributeCoins(node.right, node);
         }
 
+        // 不论向父节点借还是上交都要移动Math.abs(node.val - 1)次
         moves += Math.abs(node.val - 1);
         if (parent != null) {
+            // 标记父结点
             parent.val += node.val - 1;
         }
     }
