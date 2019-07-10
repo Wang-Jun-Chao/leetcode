59460dc7085e288acaf642cde289f2e9a775fc29
Wang-Jun-Chao <shining-glory@qq.com>
2019-07-10 13:40:06 +0800

    commit

--- [0227][Basic Calculator II]/src/Solution.java
+++ [0227][Basic Calculator II]/src/Solution.java
@@ -1,14 +1,53 @@
-import java.util.Arrays;
 import java.util.Deque;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Set;
 
 /**
+ * https://leetcode.com/problems/basic-calculator-ii/
+ *
  * @author: wangjunchao(王俊超)
  * @time: 2019-07-10 10:54
  **/
 public class Solution {
+    /**
+     * <pre>
+     * Implement a basic calculator to evaluate a simple expression string.
+     *
+     * The expression string contains only non-negative integers, +, -, *, / operators
+     * and empty spaces . The integer division should truncate toward zero.
+     *
+     * Example 1:
+     *
+     * Input: "3+2*2"
+     * Output: 7
+     * Example 2:
+     *
+     * Input: " 3/2 "
+     * Output: 1
+     * Example 3:
+     *
+     * Input: " 3+5 / 2 "
+     * Output: 5
+     * Note:
+     *
+     * You may assume that the given expression is always valid.
+     * Do not use the eval built-in library function.
+     * 思路：
+     * 遇到加减就入操作数栈和操作符栈，遇到乘除就计算，将计算结果作为新的字符串的第一个值，后面是处理的字符串
+     * 最后处理所有剩下的加减操作
+     * 如 1+2+3*4+5+6
+     * 处理完第一个*号后
+     * operator: +, +
+     * operand: 1, 2
+     * s: 12+5+6
+     * 整个字符串处理完后
+     * operator: +, +, +, +
+     * operand: 1, 2, 12, 5, 6
+     * 再进行计算
+     * </pre>
+     *
+     * @param s
+     * @return
+     */
     public int calculate(String s) {
 
         if (s == null || s.length() < 1) {
@@ -58,7 +97,7 @@ public class Solution {
                 // 比如 1*2+3 => 2+3
                 s = result + s.substring(idx);
                 idx = 0;
-            } else if (idx < s.length() && (s.charAt(idx) == '+' || s.charAt(idx) == '-')){
+            } else if (idx < s.length() && (s.charAt(idx) == '+' || s.charAt(idx) == '-')) {
                 // 如果是加减
                 operators.addLast(s.charAt(idx));
                 idx++;
