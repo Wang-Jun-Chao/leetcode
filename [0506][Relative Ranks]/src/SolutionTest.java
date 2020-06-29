1c7af47229b791a6b127eb96dfadb7c21ecd4bbf
Wang-Jun-Chao <shining-glory@qq.com>
2020-06-29 08:12:39 +0800

    commit
    Change-Id: If5826faa98325213bca778dc3fcf3c06035c00b6

---
+++ [0506][Relative Ranks]/src/SolutionTest.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME;
+
+/**
+ * Author: 王俊超
+ * Time: 2020-06-29 07:46
+ * CSDN: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/Wang-Jun-Chao
+ * Declaration: All Rights Reserved !!!
+ **/
+public class SolutionTest {
+}
